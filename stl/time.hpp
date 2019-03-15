441e3dc6a80389e12dc814512da56290cf51318a
zachary <RyanLiuF@126.com>
2019-03-15 16:44:29 +0800

    revise the tmExtend to the pod type

--- stl/time.hpp
+++ stl/time.hpp
@@ -25,10 +25,21 @@ namespace stl
             return now.time_since_epoch().count();
         }
 
-        struct tmExtend: public std::tm
+#pragma pack(push,1)
+        struct tmExtend
         {
+			int tm_sec;   // seconds after the minute - [0, 60] including leap second
+			int tm_min;   // minutes after the hour - [0, 59]
+			int tm_hour;  // hours since midnight - [0, 23]
+			int tm_mday;  // day of the month - [1, 31]
+			int tm_mon;   // months since January - [0, 11]
+			int tm_year;  // years since 1900
+			int tm_wday;  // days since Sunday - [0, 6]
+			int tm_yday;  // days since January 1 - [0, 365]
+			int tm_isdst; // daylight savings time flag
             int tm_mill;
         };
+#pragma pack(pop)
 
         inline tmExtend 
             GetLocalTime(void)
